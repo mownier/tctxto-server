9d2527d3a7ea7ebff8ed825e4c2b42b2f0ff7944
Mounir Ybanez <rinuom91@gmail.com>
2025-04-10 23:04:09 +0800

    Refactored subscribe.

--- server/subscribe.go
+++ server/subscribe.go
@@ -7,83 +7,90 @@ import (
 	status "google.golang.org/grpc/status"
 )
 
-func (s *Server) Subscribe(emp *Empty, stream TicTacToe_SubscribeServer) error {
+func (s *Server) Subscribe(stream TicTacToe_SubscribeServer) error {
 	clientId, err := s.extractClientId(stream.Context())
-
 	if err != nil {
 		return err
 	}
 
-	s.mu.Lock()
-
-	if _, exists := s.clients[clientId]; !exists {
-		s.mu.Unlock()
-
+	if !s.isClientKnown(clientId) {
 		return status.Error(codes.NotFound, "unknown client")
 	}
 
-	if _, exists := s.clientSignalMap[clientId]; !exists {
-		s.clientSignalMap[clientId] = make(chan struct{}, 1)
-	}
+	s.setupClientSubscriptionChannels(clientId)
+	s.sendInitialClientUpdates(clientId, stream)
 
-	if _, exists := s.clientUpdatesMap[clientId]; !exists {
-		s.clientUpdatesMap[clientId] = []*SubscriptionUpdate{}
-	}
+	signal := s.getClientSignalChannel(clientId)
 
-	s.clientUpdatesMap[clientId] = append(s.clientUpdatesMap[clientId], s.clientInitialUpdates(clientId)...)
-
-	signal := s.clientSignalMap[clientId]
-
-	s.mu.Unlock()
-
-	defer func() {
-		s.mu.Lock()
-		delete(s.clientSignalMap, clientId)
-		s.mu.Unlock()
-	}()
+	defer s.cleanupClientSubscription(clientId)
 
 	for {
 		select {
 		case <-stream.Context().Done():
 			return status.Error(codes.Canceled, "subscribe was cancelled")
-
 		case <-signal:
-			s.mu.Lock()
-			err := s.sendClientUpdates(clientId, stream)
-			s.mu.Unlock()
-
-			if err != nil {
+			if err := s.sendClientUpdates(clientId, stream); err != nil {
 				return err
 			}
 		}
 	}
 }
 
-func (s *Server) sendClientUpdates(clientId string, stream TicTacToe_SubscribeServer) error {
-	lastIndex, exists := s.clientLastIndexUpdate[clientId]
+func (s *Server) isClientKnown(clientId string) bool {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	_, exists := s.clients[clientId]
+	return exists
+}
 
+func (s *Server) setupClientSubscriptionChannels(clientId string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, exists := s.clientSignalMap[clientId]; !exists {
+		s.clientSignalMap[clientId] = make(chan struct{}, 1)
+	}
+	if _, exists := s.clientUpdatesMap[clientId]; !exists {
+		s.clientUpdatesMap[clientId] = []*SubscriptionUpdate{}
+	}
+}
+
+func (s *Server) getClientSignalChannel(clientId string) <-chan struct{} {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.clientSignalMap[clientId]
+}
+
+func (s *Server) sendInitialClientUpdates(clientId string, stream TicTacToe_SubscribeServer) {
+	initialUpdates := s.clientInitialUpdates(clientId)
+	s.mu.Lock()
+	s.clientUpdatesMap[clientId] = append(s.clientUpdatesMap[clientId], initialUpdates...)
+	s.mu.Unlock()
+	s.sendClientUpdates(clientId, stream)
+}
+
+func (s *Server) sendClientUpdates(clientId string, stream TicTacToe_SubscribeServer) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	lastIndex, exists := s.clientLastIndexUpdate[clientId]
 	if !exists {
 		lastIndex = -1
 	}
 
 	startIndex := 0
-
 	if lastIndex != -1 {
 		startIndex = lastIndex + 1
 	}
 
 	updatesToSend := s.clientUpdatesMap[clientId][startIndex:]
-
 	sentCount := 0
-
-	var e error
+	var err error
 
 	for _, update := range updatesToSend {
-		if err := stream.Send(update); err != nil {
-			e = err
+		if e := stream.Send(update); e != nil {
+			err = e
 			break
 		}
-
 		sentCount++
 	}
 
@@ -91,7 +98,7 @@ func (s *Server) sendClientUpdates(clientId string, stream TicTacToe_SubscribeSe
 		s.clientLastIndexUpdate[clientId] += sentCount
 	}
 
-	return e
+	return err
 }
 
 func (s *Server) clientInitialUpdates(clientId string) []*SubscriptionUpdate {
@@ -101,9 +108,7 @@ func (s *Server) clientInitialUpdates(clientId string) []*SubscriptionUpdate {
 	playerId, playerExists := s.clientPlayerMap[clientId]
 
 	if !playerExists || !s.isPlayerActive(playerId) {
-		return []*SubscriptionUpdate{
-			s.createNavigationUpdate(NavigationPath_LOGIN),
-		}
+		return []*SubscriptionUpdate{s.createNavigationUpdate(NavigationPath_LOGIN)}
 	}
 
 	if gameUpdates := s.getGameInitialUpdates(playerId); len(gameUpdates) > 0 {
@@ -114,65 +119,69 @@ func (s *Server) clientInitialUpdates(clientId string) []*SubscriptionUpdate {
 		return lobbyUpdates
 	}
 
-	return []*SubscriptionUpdate{
-		s.createNavigationUpdate(NavigationPath_HOME),
-	}
+	return []*SubscriptionUpdate{s.createNavigationUpdate(NavigationPath_HOME)}
 }
 
 func (s *Server) isPlayerActive(playerId string) bool {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
 	_, ok := s.players[playerId]
-
 	return ok
 }
 
 func (s *Server) getGameInitialUpdates(playerId string) []*SubscriptionUpdate {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	if gameId, ok := s.playerGameMap[playerId]; ok {
-		if game, ok := s.games[gameId]; ok && game.Result != models.GameResult_DRAW && game.Result != models.GameResult_WIN {
-			var you *models.Player
-			var other *models.Player
-
-			if game.MoverX.Id == playerId {
-				you = game.MoverX
-				other = game.MoverO
-			}
-
-			if game.MoverO.Id == playerId {
-				you = game.MoverO
-				other = game.MoverX
-			}
-
+		if game, ok := s.games[gameId]; ok && game.Result < models.GameResult_DRAW {
+			you, other := s.getGamePlayers(game, playerId)
 			updates := []*SubscriptionUpdate{
 				s.createNavigationUpdate(NavigationPath_GAME),
 				s.createGameStartUpdate(game, you, other),
 				s.createNextMoverUpdate(game),
 			}
-
-			updates = append(updates,
-				s.createMoveUpdates(game)...,
-			)
-
+			updates = append(updates, s.createMoveUpdates(game)...)
 			return updates
-
 		} else {
 			delete(s.playerGameMap, playerId)
 		}
 	}
-
 	return nil
 }
 
+func (s *Server) getGamePlayers(game *models.Game, playerId string) (*models.Player, *models.Player) {
+	var you *models.Player
+	var other *models.Player
+	if game.MoverX.Id == playerId {
+		you = game.MoverX
+		other = game.MoverO
+	} else if game.MoverO.Id == playerId {
+		you = game.MoverO
+		other = game.MoverX
+	}
+	return you, other
+}
+
 func (s *Server) getLobbyInitialUpdates(playerId string) []*SubscriptionUpdate {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	if lobbyId, ok := s.playerLobbyMap[playerId]; ok {
 		if lobby, ok := s.lobbies[lobbyId]; ok {
 			return []*SubscriptionUpdate{
 				s.createNavigationUpdate(NavigationPath_MY_LOBBY),
 				s.createMyLobbyDetails(lobby),
 			}
-
 		} else {
 			delete(s.playerLobbyMap, playerId)
 		}
 	}
-
 	return nil
+}
+
+func (s *Server) cleanupClientSubscription(clientId string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.clientSignalMap, clientId)
 }
